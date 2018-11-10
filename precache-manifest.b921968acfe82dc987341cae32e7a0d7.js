bd97ec2feae55118f355283051695da1a5576cb9
SIBIL7 <sibil7beats@gmail.com>
2018-11-10 17:01:37 +0530

    Updates

---
+++ precache-manifest.b921968acfe82dc987341cae32e7a0d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a642b7a5714ab840d73c",
+    "url": "/Recipe_Finder/static/js/runtime~main.a642b7a5.js"
+  },
+  {
+    "revision": "ada8aed18140bc52fa84",
+    "url": "/Recipe_Finder/static/js/main.ada8aed1.chunk.js"
+  },
+  {
+    "revision": "0cd8823fd0aec91f5ae7",
+    "url": "/Recipe_Finder/static/js/1.0cd8823f.chunk.js"
+  },
+  {
+    "revision": "ada8aed18140bc52fa84",
+    "url": "/Recipe_Finder/static/css/main.9ada9b75.chunk.css"
+  },
+  {
+    "revision": "0cd8823fd0aec91f5ae7",
+    "url": "/Recipe_Finder/static/css/1.8fcf64c3.chunk.css"
+  },
+  {
+    "revision": "6d1a821154f398285193ad21b0348f40",
+    "url": "/Recipe_Finder/index.html"
+  }
+];
